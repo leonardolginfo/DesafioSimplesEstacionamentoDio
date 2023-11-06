5bdfd1a296dbd38d2492d67b5f7c6417e34f2d75
leonardolginfo <leonardo@lginfo.com.br>
2023-11-06 17:58:53 -0300

    classe com 3 atributos iniciados

---
+++ Models/Estacionamento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleEstacionamento.Models
+{
+    internal class Estacionamento
+    {
+        private decimal precoInicial = 0;
+        private decimal precoPorHora = 0;
+        private List<string> veiculos = new List<string>();
+
+    }
+}
